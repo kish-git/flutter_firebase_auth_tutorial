a3f60438b63bcf7617bbaed3d76191f954a77f0f
kish-git <kishorraut5326@gmail.com>
2021-10-22 12:41:23 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_authentication_tutorial/authentication_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("HOME"),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<AuthenticationService>().signOut();
+                },
+                child: Text("Sign out"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
